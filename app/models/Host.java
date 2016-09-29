48a663cbac46dc2b6d554d5b64075407f77b6b5c
JDTawa <jd.torres11@uniandes.edu.co>
2016-09-28 19:34:14 -0500

    Update Host.java

--- app/models/Host.java
+++ app/models/Host.java
@@ -1,8 +1,9 @@
 package models;
 import com.avaje.ebean.Model;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 /**
  * Created by USUARIO on 28/09/2016.
  */
@@ -10,4 +11,29 @@ import javax.persistence.Table;
 @Entity
 @Table(name="hostEntity")
 public class Host extends Model {
+    public static Model.Finder<Long, Host> FINDER = new Finder<>(Host.class);
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE,generator = "Host")
+    private Long id;
+    private String name;
+
+    @ManyToOne(cascade= CascadeType.ALL)
+    @JsonBackReference
+    private PetHost pethost;
+
+    @OneToMany(mappedBy = "host")
+    private List<Calificacion > calificaciones;
+
+    @OneToMany(mappedBy = "host")
+    private List<Espacio > espacios;
+
+    @OneToOne(mappedBy = "host")
+    private Lugar lugar;
+
+    public Host()
+    {
+        this.id=null;
+        this.name ="NO NAME";
+    }
 }
