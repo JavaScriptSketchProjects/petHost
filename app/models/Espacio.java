34b1438c76a8340722e39f22ee7f0541ccb2ad21
JDTawa <jd.torres11@uniandes.edu.co>
2016-09-28 19:33:43 -0500

    Update Espacio.java

--- app/models/Espacio.java
+++ app/models/Espacio.java
@@ -1,8 +1,10 @@
 package models;
 import com.avaje.ebean.Model;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
+
 /**
  * Created by USUARIO on 28/09/2016.
  */
@@ -10,4 +12,22 @@ import javax.persistence.Table;
 @Entity
 @Table(name="espacioEntity")
 public class Espacio extends Model {
+    public static Model.Finder<Long, Espacio> FINDER = new Finder<>(Espacio.class);
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE,generator = "Espacio")
+    private Long id;
+    private String name;
+
+    @ManyToOne(cascade= CascadeType.ALL)
+    @JsonBackReference
+    private Host host;
+
+    @OneToMany(mappedBy = "espacio")
+    private List<Reserva > reservas;
+
+    public Espacio()
+    {
+        this.id=null;
+        this.name ="NO NAME";
+    }
 }
