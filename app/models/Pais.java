4f91fd37bc9299175fce66bcadb14c889f51277f
JDTawa <jd.torres11@uniandes.edu.co>
2016-09-28 21:38:20 -0500

    Update Enum Paises

--- app/models/Pais.java
+++ app/models/Pais.java
@@ -4,4 +4,385 @@ package models;
  * Created by USUARIO on 28/09/2016.
  */
 public enum Pais {
+    Afghanistán,
+
+    Albania,
+
+    Alemania,
+
+    Andorra,
+
+    Angola,
+
+    Anguilla,
+
+    Antigua_Barbuda,
+
+    Arabia_Saudita,
+
+    Argelia,
+
+    Argentina,
+
+    Armenia,
+
+    Australia,
+
+    Austria,
+
+    Azerbayán,
+
+    Bahamas,
+
+    Bahrein,
+
+    Bangladesh,
+
+    Barbados,
+
+    Belarús,
+
+    Bélgica,
+
+    Belice,
+
+    Benin,
+
+    Bhután,
+
+    Birmania,
+
+    Bolivia,
+
+    Bosnia_Herzegovina,
+
+    Botswana,
+
+    Brasil,
+
+    Brunei,
+
+    Bulgaria,
+
+    Burkina_Faso,
+
+    Burundi,
+
+    Cabo_Verde,
+
+    Camboya,
+
+    Camerún,
+
+    Canadá,
+
+    Chad,
+
+    Chile,
+
+    China,
+
+    Chipre,
+
+    Colombia,
+
+    Comoras,
+
+    Congo,
+
+    Congo_R_D,
+
+    Corea_del_Norte,
+
+    Corea_del_Sur,
+
+    Costa_de_Marfil,
+
+    Costa_Rica,
+
+    Croacia,
+
+    Cuba,
+
+    Dinamarca,
+
+    Djibouti,
+
+    Ecuador,
+
+    Egipto,
+
+    El_Salvador,
+
+    Emiratos_Arabes_Unidos,
+
+    Eritrea,
+
+    Eslovaquia,
+
+    Eslovenia,
+
+    España,
+
+    Estados_Unidos,
+
+    Estonia,
+
+    Etiopia,
+
+    Fiji,
+
+    Filipinas,
+
+    Finlandia,
+
+    Francia,
+
+    Gabon,
+
+    Gambia,
+
+    Georgia,
+
+    Ghana,
+
+    Grecia,
+
+    Granada,
+
+    Guatemala,
+
+    Guinea_Bissau,
+
+    Guinea_Ecuatorial,
+
+    Guyana,
+
+    Haiti,
+
+    Honduras,
+
+    Hungria,
+
+    Islandia,
+
+    India,
+
+    Indonesia,
+
+    Iran,
+
+    Iraq,
+
+    Irlanda,
+
+    Israel,
+
+    Italia,
+
+    Jamaica,
+
+    Japón,
+
+    Jordania,
+
+    Kazajstán,
+
+    Kenia,
+
+    Kirguistán,
+
+    Kiribati,
+
+    Kuwait,
+
+    Laos,
+
+    Letonia,
+
+    Libano,
+
+    Lesotho,
+
+    Liberia,
+
+    Libia,
+
+    Liechtenstein,
+
+    Lituania,
+
+    Luxemburgo,
+
+    Macedonia,
+
+    Madagascar,
+
+    Malawi,
+
+    Malasia,
+
+    Maldivas,
+
+    Mali,
+
+    Malta,
+
+    Marruecos,
+
+    Marshall,
+
+    Mauricio,
+
+    Mauritania,
+
+    México,
+
+    Micronesia,
+
+    Moldova,
+
+    Mónaco,
+
+    Mongolia,
+
+    Mozambique,
+
+    Namibia,
+
+    Naurú,
+
+    Nepal,
+
+    Nicaragua,
+
+    Niger,
+
+    Nigeria,
+
+    Noruega,
+
+    Nueva_Zelandia,
+
+    Omán,
+
+    Países_Bajos,
+
+    Pakistán,
+
+    Palau,
+
+    Panamá,
+
+    Papúa_Nueva_Guinea,
+
+    Paraguay,
+
+    Perú,
+
+    Polonia,
+
+    Portugal,
+
+    Qatar,
+
+    Reino_Unido,
+
+    Rep_Centroafricana,
+
+    Rep_Checa,
+
+    Rep_Dominicana,
+
+    Ruanda,
+
+    Rumania,
+
+    Rusia,
+
+    Salomon_Islands,
+
+    Samoa,
+
+    San_Marino,
+
+    San_Cristóbal_Nevis,
+
+    Santa_Lucía,
+
+    Santa_Sede,
+
+    São_Tomé_Principe,
+
+    St_Vincente_las_Grenadinas,
+
+    Senegal,
+
+    Seychelles,
+
+    Sierra_Leona,
+
+    Singapur,
+
+    Siria,
+
+    Somalia,
+
+    Sri_Lanka,
+
+    Sudáfrica,
+
+    Sudán,
+
+    Suecia,
+
+    Suiza,
+
+    Suriname,
+
+    Swazilandia,
+
+    Tailandia,
+
+    Taiwán,
+
+    Tanzania,
+
+    Tayikistán,
+
+    Togo,
+
+    Tonga,
+
+    Trinidad_Tabago,
+
+    Túnez,
+
+    Turkmenistan,
+
+    Turquia,
+
+    Tuvalu,
+
+    Ucraina,
+
+    Uganda,
+
+    Uruguay,
+
+    Uzbekistán,
+
+    Vanuatu,
+
+    Venezuela,
+
+    Vietnam,
+
+    Yémen,
+
+    Yugoslavia,
+
+    Zambia,
+
+    Zimbabwe
 }
